5bb390a6d0dceba239f32318d05709ad7ff7d7d8
JBs197 <75170573+JBs197@users.noreply.github.com>
2021-03-10 08:21:11 -0800

    Update catalogue.h
    New region functions.

--- SCDAgui/catalogue.h
+++ SCDAgui/catalogue.h
@@ -9,12 +9,24 @@ class CATALOGUE
 {
     CSV model;
     QString qpath;
+    std::wstring wpath;#ifndef CATALOGUE_H
+#define CATALOGUE_H
+
+#include <QtSql>
+#include <QtConcurrent>
+#include "csv.h"
+
+class CATALOGUE
+{
+    CSV model;
+    QString qpath;
     std::wstring wpath;
     QString qname;
     QString tname;
     string sname;
     wstring wname;
     QString qyear;
+    string syear;
     bool multi_column;
     QString qdescription;
     wstring wfile;
@@ -44,23 +56,21 @@ public:
     void set_path(QString&);
     void initialize_table();
     void make_name_tree();
-    QString sublabelmaker(QString&, QVector<QVector<int>>&);
+    string sublabelmaker(string&, QVector<QVector<int>>&);
     wstring get_csv_path(int);
     QString get_csv_branch(int);
     QString get_qname();
-    QString get_gid(int);
+    string get_sname();
+    string get_gid(int);
     int get_gid_size();
-    QVector<QString> get_create_table_statements(int);
-    QVector<QString> get_CSV_insert_value_statements(int);
-    void create_table_taskmaster(QVector<QString>&, int);
-    void create_table_mapped(QVector<QString>&);
-    QString create_primary_table();
-    QString get_create_sub_template();
-    QString get_insert_csv_row_template();
+    string create_primary_table();
+    string create_column_table();
+    string get_create_sub_template();
+    string get_insert_csv_row_template();
     QVector<QVector<QVector<int>>> get_tree();
     QVector<QString> get_gid_list();
     void insert_primary_columns_template();
-    QString get_primary_template();
+    string get_primary_template();
     void set_primary_columns_template(QString);
     void set_tree(QVector<QVector<QVector<int>>>);
     void set_gid_list(QVector<QString>);
@@ -72,18 +82,21 @@ public:
     void set_multicol(bool);
     void set_qfile(int);
     QVector<QVector<QString>> extract_data_rows(QString&, int&);
+    vector<vector<string>> extract_data_rows8(string&, int&);
     QVector<QString> get_column_titles();
     void set_column_titles(QVector<QString>);
     QVector<QString> get_row_titles();
     void set_row_titles(QVector<QString>);
     QVector<QVector<QString>> get_model_text_variables();
-    QString get_year();
+    QString get_qyear();
+    string get_syear();
     void set_description(QString);
     QString get_description();
     void basic_input(QVector<QString>);
     void create_csv_tables_template();
-    QString get_csv_template();
+    string get_csv_template();
     QVector<QVector<QString>> extract_text_vars(QString&);
+    vector<vector<string>> extract_text_vars8(string&);
     void insert_csv_row_template();
     void print_stuff();
     void cancel_insertion();
